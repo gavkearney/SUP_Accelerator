2cbb8db68fe8e4e8f1e09e0224b2fd440b04e778
Trey Pringle <smpringl@gmail.com>
2014-12-18 10:53:36 -0300

    changed file to php

---
+++ index.php
@@ -0,0 +1,168 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        
+        <title>Oso Acclerator</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="css/flexslider.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/line-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/elegant-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/lightbox.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/theme-aquatica.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,600,700%7CRaleway:700' rel='stylesheet' type='text/css'>
+        <script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+        <style>
+        .message {padding:20px 0px 50px;text-align:center;margin-bottom:50px;border-bottom:1px solid #e6e6e6;}
+        .copyright {margin-top:10px;font-size:12px;color:#808080;}
+        .gr-bg {background:#e6e6e6;}
+        .text-center {text-align: center;}
+        a:link, a:hover, a:active, a:visited {color:#808080;font-size: 20px;}
+        </style>
+    </head>
+    <body>
+    	<div class="loader">
+    		<div class="spinner">
+			  <div class="double-bounce1"></div>
+			  <div class="double-bounce2"></div>
+			</div>
+    	</div>
+				
+		
+		
+		<div class="main-container">
+		<section class="hero-slider">
+				<ul class="slides">
+					<li class="overlay">
+						<div class="background-image-holder parallax-background">
+							<img alt="Background Image" src="img/hero4.jpg">
+						</div>
+						
+						<div class="video-wrapper">
+							<video autoplay="" muted="" loop="">
+								<source src="video/video.webm" type="video/webm">
+								<source src="video/video.mp4" type="video/mp4">
+								<source src="video/video.ogv" type="video/ogg">	
+							</video>
+						</div>
+						
+						<div class="container align-vertical">
+							<div class="row">
+								<div class="col-lg-12" style="text-align:center;">
+									<img class="logo" alt="Logo" src="img/speechbubble.png" style="height:200px;">
+								</div>
+								<div class="col-lg-12 text-center">
+									<h1 class="text-white">Main tag line goes here.<div>Applications now open...</div></h1>
+									<!--<a target="_blank" href="http://www.mediumra.re/pivot/variant/builder.html" class="btn btn-primary btn-white">Customize Pivot</a>-->
+									<a href="#" class="btn btn-primary btn-filled">Apply</a>
+								</div>
+							</div>
+						</div><!--end of container-->
+					</li><!--end of individual slide-->
+					
+					<!--end of individual slide-->
+		
+				</ul>
+			</section>
+			
+			
+			
+			<section class="duplicatable-content">
+			
+				<div class="container">
+					<!--end of row-->
+		
+					<div class="row">
+						<div class="col-sm-10 col-sm-offset-1 message">
+							<h2>The OSO Accelerator is a program focused on moving early stage Chilean start-ups into top tier accelerator programs around the world.</h2>
+						</div>
+						<div class="col-sm-4">
+							<div class="feature feature-icon-large">
+								<div class="pull-left">
+									<i class="icon icon-genius"></i>
+								</div>
+								<div class="pull-right">
+									<h5>8 week program</h5>
+									<p>
+										Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.
+									</p>
+								</div>
+							</div>
+						</div><!--end 6 col-->
+						
+						<div class="col-sm-4">
+							<div class="feature feature-icon-large">
+								<div class="pull-left">
+									<i class="icon icon-linegraph"></i>
+								</div>
+								<div class="pull-right">
+									<h5>Experienced Mentorship</h5>
+									<p>
+										Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.
+									</p>
+								</div>
+							</div>
+						</div><!--end 6 col-->
+						
+						<div class="col-sm-4">
+							<div class="feature feature-icon-large">
+								<div class="pull-left">
+									<i class="icon icon-tools-2"></i>
+								</div>
+								<div class="pull-right">
+									<h5>Forward Motion</h5>
+									<p>
+										Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.
+									</p>
+								</div>
+							</div>
+						</div><!--end 6 col-->
+						
+						<!--end 6 col-->
+						
+					</div><!--end of row-->
+				</div>
+			
+			</section>
+		</div>
+		
+		<div class="footer-container">
+		
+			<footer class="short gr-bg">
+				<div class="container">
+					<div class="row text-center">
+						<div class="col-sm-12">
+							<a href="">Apply Today</a>
+							<p class="copyright">&copy; 2014 OSO</p>
+						</div>
+						
+						
+					</div><!--end of row-->
+				</div><!--end of container-->
+			</footer>
+		</div>
+				
+		<script src="js/jquery.min.js"></script>
+        <script src="js/jquery.plugin.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.flexslider-min.js"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+        <script src="js/skrollr.min.js"></script>
+        <script src="js/spectragram.min.js"></script>
+        <script src="js/scrollReveal.min.js"></script>
+        <script src="js/isotope.min.js"></script>
+        <script src="js/twitterFetcher_v10_min.js"></script>
+        <script src="js/lightbox.min.js"></script>
+        <script src="js/jquery.countdown.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+				
